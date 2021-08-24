51d53580e412203645dc2caccc47831dcfcd087f
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-08-24 15:07:21 +0100

    Add an array of object in index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style/style.css';
+
+const tasks = [
+  {
+    description: 'Go to school',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Do chores',
+    completed: false,
+    index: 3,
+  },
+];
